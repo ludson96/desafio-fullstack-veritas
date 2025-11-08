767ac73c080021ac7605f27a27ca438bd1a0c19e
ludson96 <ludson_ps27@hotmail.com>
2025-11-08 11:11:43 -0300

    feat: :sparkles: add method Post for create task in /tasks

--- backend/handlers.go
+++ backend/handlers.go
@@ -13,3 +13,21 @@ func getTasks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(tasks)
 }
+
+// POST /tasks
+func createTask(w http.ResponseWriter, r *http.Request) {
+	var newTask Task
+	json.NewDecoder(r.Body).Decode(&newTask)
+
+	if err := validateTask(newTask); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	newTask.ID = nextID
+	nextID++
+	tasks = append(tasks, newTask)
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newTask)
+}
